6a7c03cff0236e61b84ed166db46e29d53a55e84
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2021-05-15 14:36:34 +0200

    feat: PresentForm appearance

--- src/components/PresentForm/index.js
+++ src/components/PresentForm/index.js
@@ -7,6 +7,19 @@ export default function PresentForm() {
             <h1 className="title"><strong>Welk cadeautje kies je?*</strong></h1>
             <p className="text">Selecteer de afbeelding met het cadeautje dat je 
             wenst te ontvangen na het plaatsen van een review!</p>
+
+            <div className="sd-border">
+                <div className="sd-image-container">
+                    <img className="sd-image" src='/images/SD-kaart.png' alt="SD-Kaart"/>
+                </div>
+                <p className="image-text">16 GB SD-kaart</p>
+            </div>
+            <div className="mousepad-border">
+                <div className="mousepad-image-container">
+                    <img className="mousepad-image" src='/images/muismat.jpg' alt="Ergonomische muismat"/>
+                </div>
+                <p className="image-text">Ergonomische muismat</p>
+            </div>
         </div>
     )
 }
